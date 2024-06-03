ba7d86672d8f5a423050e4aaec3c3722e406c0c5
daveqwerty1007 <jianbo.zhao1007@gmail.com>
2024-06-03 18:38:35 -0400

    Update week1

--- week1.py
+++ week1.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-
-data = pd.read_csv('/Users/zhaojianbo/Desktop/creditcard.csv')
+path = '/Users/zhaojianbo/Desktop/Alphabet/Week1/creditcard.csv'
+data = pd.read_csv(path)
 
 
 ##print(data.head())
@@ -49,4 +52,25 @@ predictions = ppn.predict(X)
 print("预测:", predictions)
 print("accuracy:", accuracy_score(y, ppn.predict(X)))
 
-## Nerual
+## model setup
+model = Sequential()
+model.add(Dense(32, input_dim=train_X.shape[1], activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_X, train_y, epochs=10, batch_size=32, validation_split=0.1)
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(test_X, test_y)
+print("Neural Network Test Accuracy:", test_accuracy)
+
+# Make predictions on the test set
+nn_predictions = (model.predict(test_X) > 0.5).astype("int32")
+
+# Evaluate the model
+nn_accuracy = accuracy_score(test_y, nn_predictions)
+print("Neural Network Accuracy:", nn_accuracy)
